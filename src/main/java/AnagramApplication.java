2112e1aed49be10535f8fe748e961eed8643b95f
seregia <seregia@gmail.com>
2019-03-02 14:43:20 +0200

    Stabilization and performance improvements

--- src/main/java/AnagramApplication.java
+++ src/main/java/AnagramApplication.java
@@ -1,9 +1,6 @@
-import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
 
@@ -14,6 +11,17 @@ class AnagramApplication {
     private static final String FALLBACK_FILEPATH = "/home/seregia/Documents/projects/java/anagramApplication/src/main/resources/lemmad.txt";
     private static final String FALLBACK_WORD = "vits";
 
+    private static byte[] fileData;
+    private static byte[] searchWord;
+    
+    public static void setFileData(byte[] fileDataArg) {
+    	fileData = fileDataArg;
+    }
+    
+    public static void setSearchWord(byte[] searchWordArg) {
+    	searchWord = searchWordArg;
+    }
+    
 	public static void main(String[] argv) {
 		
 		final String filePath = argv.length >= 1 && !Strings.isNullOrEmpty(argv[0])
@@ -30,13 +38,11 @@ class AnagramApplication {
 			return;
 		}
 
-		
-			
 	    long startTime = System.currentTimeMillis();
 	    String result = "";
 	    
 	    File file = new File(filePath);
-	    byte[] fileData = new byte[(int) file.length()];
+	    fileData = new byte[(int) file.length()];
 	    DataInputStream dis = null;
 		try {
 			dis = new DataInputStream(new FileInputStream(file));
@@ -53,53 +59,74 @@ class AnagramApplication {
 			}
 		}
 		
-		
-		//reovesi
-		
-		//byte[] searchWord = {'r','e','o','v','e','s','i'};
-		//byte[] searchWord = {'m','u','r','d','j','a'};
-		byte[] searchWord = "vastupidavustreening".getBytes(Charset.forName("CP1257"));
+		searchWord = "poleervedelik".getBytes(Charset.forName("CP1257"));
 
+		sort(searchWord, 0, searchWord.length);
+		findAnagram(new byte[]{});
 		
-		recursion(fileData, searchWord, new byte[]{});
-		
-		
-		//printFromBuffer(fileData,
-		//		locateSolidWord(fileData, fileData.length / 2));
-		 
 		long stop = System.currentTimeMillis() - startTime;
 	    System.out.println(stop + result);
 	}
 
-	private static void recursion(byte[] fileData, byte[] searchWord, byte[] maskedChars) {
+	
+	private static void swap(byte[] x, int a, int b) {
+		byte t = x[a];
+		x[a] = x[b];
+		x[b] = t;
+	}
+	
+	private static void sort(byte[] dest, int low, int high) {
+         for (int i=low; i<high; i++)
+             for (int j=i; j>low && (((char)dest[j-1]) > ((char)dest[j])); j--)
+                 swap(dest, j, j-1);
+         return;
+ 
+	}
+	
+	public static void findAnagram(byte[] maskedChars) {
 		
 		byte[] maskedWord = applyMask(searchWord, maskedChars);
-		addTabs(maskedChars);
-		System.out.println("D: " + maskedChars.length + " MW: " + new String(maskedWord) + " M: " + new String(maskedChars));
+	
+		//addTabs(maskedChars);
+		//System.out.println("D: " + maskedChars.length + " MW: " + new String(maskedWord) + " M: " + new String(maskedChars));
+		char lastCharFromPreviousResult = 0;
 		for (int i = 0; i < maskedWord.length; i++) {
 			byte b = maskedWord[i];
-			
-			boolean contains = false;
-			for (int j = 0; j < i && !contains; j++) {
-				contains = b == maskedWord[j];
-			}
-			
-			if (!contains) {
-				byte[] query = addToMask(b, maskedChars);
+
+			if ((char)b >= lastCharFromPreviousResult) {
 				
-				int responseIdx = locatePositionByString(fileData, query);
-				addTabs(maskedChars);
-				System.out.print("q: " + new String(query) + " res: ");
-				printFromBuffer(fileData, responseIdx);
-	
-				if (query.length == searchWord.length) {
-					if (0 == stringCompare(fileData, responseIdx, query)) {
-						addTabs(maskedChars);
-						System.out.print("RESULT -> ");
-						printFromBuffer(fileData, responseIdx);
+				// Optimization: Skip identical branches starting on same letter
+				boolean contains = false;
+				for (int j = 0; j < i && !contains; j++) {
+					contains = b == maskedWord[j];
+				}
+				
+				if (!contains) {
+					byte[] query = addToMask(maskedChars, b);
+					
+					int responseIdx = locatePositionByString(fileData, query);
+					//addTabs(maskedChars);
+					//System.out.print("q: " + new String(query) + " res: ");
+					//printFromBuffer(fileData, responseIdx);
+					
+					// Optimization: Skip all following branches if response doesn't match to root criteria 
+					if (0 != stringCompareWithMaxLen(fileData, responseIdx, query, maskedChars.length)) {
+						return;
 					}
-				} else if (0 == stringCompareWithMaxLen(fileData, responseIdx, query, query.length)) {
-					recursion(fileData, searchWord, query);
+					
+					lastCharFromPreviousResult = (char)fileData[responseIdx + maskedChars.length];
+					
+					if (query.length == searchWord.length) {
+						if (0 == stringCompare(fileData, responseIdx, query)) {
+							System.out.print("RESULT -> ");
+							printFromBuffer(fileData, responseIdx);
+						}
+					}
+					
+					// Optimization: Continue recursion if response starts from criteria 
+					else if (0 == stringCompareWithMaxLen(fileData, responseIdx, query, query.length)) {
+						findAnagram(query);
+					}
 				}
 			}
 		}
@@ -111,7 +138,7 @@ class AnagramApplication {
 		}
 	}
 
-	private static byte[] addToMask(byte b, byte[] maskedChars) {
+	private static byte[] addToMask(byte[] maskedChars, byte b) {
 		byte[] ret = new byte[maskedChars.length + 1];
 		System.arraycopy(maskedChars, 0, ret, 0, maskedChars.length);
 		ret[ret.length - 1] = b;
@@ -146,30 +173,33 @@ class AnagramApplication {
 		return maskedWord;
 	}
 
-	private static int locatePositionByString(byte[] fileData, byte[] searchWord) {
+	public static int locatePositionByString(byte[] fileData, byte[] searchWord) {
 		int upperBound = fileData.length - 1;
 		int lowerBound = 0; 
 		
 		while (true) {
-			int tryPostion = lowerBound + (upperBound - lowerBound) / 2;
-			//System.out.println(" T: " + tryPostion);
+			int tryPostion = (int)Math.round((lowerBound + (upperBound - lowerBound) / 2.0));
+//			System.out.println(" T: " + tryPostion);
 			
-			tryPostion = locateSolidWord(fileData, tryPostion);
-			//printFromBuffer(fileData, tryPostion);
-			int compareResult = stringCompare(fileData, tryPostion, searchWord);
-			//System.out.println(" CMP: " + compareResult);
+			int wordPostion = locateSolidWord(fileData, tryPostion);
+			//tryPostion = wordPostion;
+			int compareResult = stringCompare(fileData, wordPostion, searchWord);
+//			printFromBuffer(fileData, wordPostion);
+//			System.out.print(" CMP: " + compareResult + " ");
+//			printFromBuffer(searchWord, 0);
+//			System.out.println("");
 			if (compareResult > 0) {
 				if(upperBound == tryPostion) {
-					return tryPostion;
+					return wordPostion;
 				}
 				upperBound = tryPostion;
 			} else if (compareResult < 0) {				
 				if(lowerBound == tryPostion) {
-					return tryPostion;
+					return wordPostion;
 				}
 				lowerBound = tryPostion;
 			} else {
-				return tryPostion;
+				return wordPostion;
 			}
 			//System.out.println(" U " + upperBound + " L " + lowerBound);
 		}
@@ -184,7 +214,7 @@ class AnagramApplication {
 		return b == '\n' || b == '\r';
 	}
 	
-	 private static int stringSize(byte[] str1, int str1Offset) { 
+	 public static int stringSize(byte[] str1, int str1Offset) { 
     	int i = 0;
         while (!isEndOfString(str1, str1Offset + i) && i + str1Offset < str1.length) {
             i++;
@@ -221,28 +251,37 @@ class AnagramApplication {
         }
     }
     
-	private static void printFromBuffer(byte[] buffer, int offset) {
+	public static void printFromBuffer(byte[] buffer, int offset) {
 		int m = offset;
 		while (m < buffer.length) {
 			System.out.print(new String(new byte[]{buffer[m++]}, Charset.forName("CP1257")));
 			//System.out.print(Character. buffer[m++]));
-			if (buffer[m] == '\n' || buffer[m] == '\r') {
+			if (m < buffer.length && (buffer[m] == '\n' || buffer[m] == '\r')) {
 				System.out.println();
 				break;
 			}
 		}
 	}
 	
-	private static int locateSolidWord(byte[] buffer, int offset) {
-		boolean nextLineFound = offset == 0;
-		for (int i = offset; i < buffer.length; i++) {
-			byte b = buffer[i];
-			
-			if (b == '\n' || b == '\r') {
-				nextLineFound = true;
+	public static int locateSolidWord(byte[] buffer, int offset) {
+		
+		for (int i = 0; i + offset < buffer.length; i++) {
+			int wordEdge;
+			if (offset - i == 0) {
+				wordEdge = -1;
+			} else if (isEndOfString(buffer, offset - i)) {
+				wordEdge = -1;
+			} else if (isEndOfString(buffer, offset + i)) {
+				wordEdge = 1;
+			} else {
+				wordEdge = 0;
 			}
-			else if (nextLineFound) {
-				return i;
+
+			for (int j = wordEdge > 0 ? offset + i : offset - i; j < buffer.length && wordEdge != 0; j++) {
+				if (!isEndOfString(buffer, j)){
+					return j;
+				}
+					
 			}
 		}
 		return -1;
