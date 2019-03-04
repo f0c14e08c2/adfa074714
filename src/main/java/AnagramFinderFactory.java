22f6850524c354a4fa2edcb7075d417d8db57216
seregia <seregia@gmail.com>
2019-03-05 01:38:21 +0200

    Added comments

--- src/main/java/AnagramFinderFactory.java
+++ src/main/java/AnagramFinderFactory.java
@@ -6,23 +6,28 @@ public class AnagramFinderFactory {
     
     private static final int EMPERICAL_THRESHOLD = 9;
     public AnagramFinder getAnagramFinder(byte[] searchWord) {
+    	
+    	// Optimization: Skip whole validation if only one character is entered
     	if (searchWord.length < 2) {
     		return null;
     	}
 
-    	ByteStringUtils.sort(searchWord, 0, searchWord.length);
+    	byte[] searchWordSorted = new byte[searchWord.length];
+		System.arraycopy(searchWord, 0, searchWordSorted, 0, searchWordSorted.length);
+    	ByteStringUtils.sort(searchWordSorted, 0, searchWordSorted.length);
     	
-    	int uniqueCharsSize = searchWord.length > 0 ? 1 : 0;
-    	for (int i = 0; i < searchWord.length - 1; i++) {
-			if (searchWord[i] != searchWord[i + 1]) {
+    	int uniqueCharsSize = 1;
+    	for (int i = 0; i < searchWordSorted.length - 1; i++) {
+			if (searchWordSorted[i] != searchWordSorted[i + 1]) {
 				uniqueCharsSize++;
 			}
 		}
     	
+    	// Optimization: Skip whole validation if only one unique character is entered
     	if (uniqueCharsSize < 2) {
     		return null;
     	}
-
+    	
     	return uniqueCharsSize > EMPERICAL_THRESHOLD
     			? sequentialAnagramFinder 
     			: btreeAnagramFinder;
